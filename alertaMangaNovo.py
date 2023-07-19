8ef1632b84773c99b8dcf3c95592e1c454f16095
Christian Carvalho <christian.coliveira@gmail.com>
2023-07-19 18:48:53 -0300

    alerta mangas corrigido

--- alertaMangaNovo.py
+++ alertaMangaNovo.py
@@ -1,6 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import colorama
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
-import time
 from time import sleep
 from datetime import date
 import telepot
@@ -8,8 +12,9 @@ from natsort import natsorted
 import platform
 import requests
 from urllib.parse import quote
-import sqlite3
-
+import zipfile
+import zipfile as zipf
+import os
 sistema = platform.system()
 
 # busca o manga
@@ -23,174 +28,38 @@ else:
 token = '5511090444:AAGGloPQ-Qh4Fwz0xgpP5rR-Yvc1nuWNK6A'
 bot = telepot.Bot(token)
 
-links = ['https://unionleitor.top/manga/dandadan', 'https://unionleitor.top/pagina-manga/one-punch-man',
-         'https://unionleitor.top/manga/chainsaw-man', 'https://unionleitor.top/manga/mashle-magic-and-muscles',
+links = ['https://unionleitor.top/manga/dandadan',
+         'https://unionleitor.top/manga/chainsaw-man', 'https://unionleitor.top/pagina-manga/one-punch-man',
+         'https://unionleitor.top/manga/mashle-magic-and-muscles',
          'https://unionleitor.top/pagina-manga/solo-leveling', 'https://mangaschan.com/manga/jujutsu-kaisen/', 'https://mangaschan.com/manga/tokyo-revengers/', 'https://mangaschan.com/manga/chainsaw-man/', 'https://mangaschan.com/manga/black-clover/', 'https://mangaschan.com/manga/sakamoto-days/', 'https://mangaschan.com/manga/mashle-magic-and-muscles/', 'https://mangaschan.com/manga/dandadan/']
 
 envio = [769723764]  # , 1299478866]
 # conn = sqlite3.connect('manga.db')
-# cursor = conn.cursor()
 
 
-# def buscaNovidade(linkmanga, fonte):
-#     today = date.today()
-#     d1 = today.strftime("%d-%m-%y")
-
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless=True)
-#         page = browser.new_page()
-#         page.goto(linkmanga)
-
-#         time.sleep(2)
-#         capitulos = page.content()
-#         page.close()
-
-#         capitulos = BeautifulSoup(capitulos, 'html.parser')
-#         data = None
-#         # cria a lista de capitulos
-
-#         if fonte == 'mangayabu':
-#             lista = capitulos.find('div', {'class': 'row rowls'})
-#             fotos = capitulos.find('div', {'class': 'single-bg'})
-
-#             # salva a capa do manga
-#             foto = fotos.find('img').get('src')
-
-#         elif fonte == 'scanlator':
-#             lista = capitulos.find_all('li', {'class': 'wp-manga-chapter'})
-#             fotos = capitulos.find('div', {'class': 'summary_image'})
-
-#             # salva a capa do manga
-#             foto = fotos.find('img').get('data-src')
-
-#         elif fonte == 'muito manga':
-#             lista = capitulos.find_all('div', {'class': 'single-chapter'})
-#             fotos = capitulos.find('div', {'class': 'capaMangaInfo'})
-
-#             # salva a capa do manga
-#             foto = fotos.find('img').get('data-src')
-#         elif fonte == 'lermanga':
-#             lista = capitulos.find('div', {'class': 'manga-chapters'})
-#             fotos = capitulos.find('div', {'class': 'capaMangaInfo'})
-
-#             # salva a capa do manga
-#             foto = fotos.find('img').get('src')
-
-#         elif fonte == 'union':
-#             lista = capitulos.find_all('div', {'class': 'row capitulos'})
-#             # fotos = capitulos.find(
-#             #     'div', {'class': 'col-md-4 col-xs-12 text-center col-md-perfil'})
-
-#             # salva a capa do manga
-#             # foto = fotos.find(
-#             #     'img').get('src')
-
-#         elif fonte == 'firemangas':
-#             lista = capitulos.find_all(
-#                 'a', {'class': 'link-dark'})
-#             fotos = capitulos.find('div', {'class': 'thumb'})
-
-#             # salva a capa do manga
-#             foto = fotos.find(
-#                 'img').get('src')
-
-#         for item in lista:
-#             # separa o nome do link do capitulo
-#             cap = BeautifulSoup(str(item), 'html.parser')
-#             if fonte == 'mangayabu':
-#                 nome = cap.find(
-#                     'div', {'class': 'manga-index-header'}).text
-#                 link = cap.find('a').get('href')
-#                 if not data:
-#                     data = nome
-#                 nome = nome.replace(
-#                     "Capí­tulo ", "").replace('• ', '').replace('Capítulo:', '').split()[0]
-
-#             elif fonte == 'scanlator':
-#                 nome = cap.find('a').text.strip()
-#                 link = cap.find('a').get('href')
-#                 if not data:
-#                     data = cap.find('a').text
-#                 nome = nome.split('-')[0].strip().replace('Capítulo', '').replace(
-#                     '-', '.').replace(',', '.').replace('v', '.').replace('?', '')
-
-#             elif fonte == 'muito manga':
-#                 nome = cap.find('div', {'class': 'single-chapter'}).text
-#                 link = 'https://muitomanga.com' + \
-#                     str(cap.find('a').get('href'))
-#                 if not data:
-#                     data = cap.find('small').text.replace('?', '')
-#                 nome = nome.split('#')[1]
-#                 nome = nome.split('\n')[0]
-
-#             elif fonte == 'lermanga':
-#                 nome = cap.find(
-#                     'a', {'class': 'dynamic-visited'}).get('title')
-#                 link = cap.find('a').get('href')
-#                 if not data:
-#                     data = nome
-
-#                 nome = nome.replace(
-#                     "Capítulo ", "").replace(' Ler Mangá', '').replace(' ', '').split('–')[1]
-#             elif fonte == 'union':
-#                 nome = cap.find('a').text
-#                 link = cap.find('a').get('href')
-#                 # print(nome, link)
-#                 if not data:
-#                     data = nome
-
-#                 nome = nome.replace('Cap. ', '')
-
-#             elif fonte == 'firemangas':
-#                 nome = cap.find('a').text
-#                 nome = nome.replace('VistoCapítulo ', '').split(' ')[0]
-#                 link = cap.find('a').get('href')
-#                 # print(nome, link)
-#                 if not data:
-#                     data = nome
-
-#                 nome = nome.replace('Cap. ', '')
-
-#         # data = data.replace('/', '-')
-#         # print(linkmanga, str(data).strip(), d1)
-#         return linkmanga, data
+# abre as configs do chrome
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_experimental_option(
+    "excludeSwitches", ['enable-logging'])
 
 
-# txt = ''
+def limpaArquivos(id, pasta, ext):
+    print('>>> funcoes - limpaTXT')
+    import os
+    import glob
+    # Get a list of all the file paths that ends with .txt from in specified directory
+    if pasta == 'Download':
+        fileList = glob.glob(f'Download/{id}/*.{ext}')
+    elif pasta == 'TXT':
+        fileList = glob.glob(f'TXT/{id}*.{ext}')
 
-# with open('/Scripts/recente.txt', 'a', encoding='utf-8') as f:
-#     with open('/Scripts/recente.txt', 'r', encoding='utf-8') as fr:
-#         fr = fr.read()
-#         for link in links:
-
-#             if 'scanlator' in link:
-#                 fonte = 'scanlator'
-#             elif 'muitomanga' in link:
-#                 fonte = 'muito manga'
-#             elif 'union' in link:
-#                 fonte = 'union'
-#             elif 'lermanga' in link:
-#                 fonte = 'lermanga'
-#             elif 'firemangas' in link:
-#                 fonte = 'firemangas'
-#             try:
-#                 manga, data = buscaNovidade(link, fonte)
-
-#                 if not f'{link},{str(data).strip()}\n' in fr:
-#                     f.write(f'{link},{str(data).strip()}\n')
-#                     for u in envio:
-#                         bot.sendMessage(
-#                             int(u), f'{link},{str(data).strip()}\n')
-#                 # f.write(f'{link}, {manga}, {str(data).strip()}\n')
-#                 # log.write(
-#                 #     f'{date.today()}-ADD-{link}, {manga}, {str(data).strip()}\n')
-#             except Exception as e:
-#                 print(f'{date.today()}-ERR-{link=} {e=}\n')
-#             try:
-#                 if manga:
-#                     txt += manga + data + '\n\n'
-#             except:
-#                 print('erro')
+    # Iterate over the list of filepaths & remove each file.
+    for filePath in fileList:
+        try:
+            os.remove(filePath)
+        except:
+            print("Error while deleting file : ", filePath)
 
 
 def getCapitulosFromUrl(link):
@@ -229,16 +98,129 @@ def getCapitulosFromUrl(link):
         except:
             pass
     # print(capitulos)
+    print(f'{capitulos[0][0]} {capitulos[0][1]}')
     return f'{capitulos[0][0]} {capitulos[0][1]}'
 
 
-with open('/Scripts/recente.txt', 'a', encoding='utf-8') as f:
-    with open('/Scripts/recente.txt', 'r', encoding='utf-8') as fr:
+def getImgFromUrl(id, mangaName, servidor, url):
+
+    try:
+        os.mkdir(f'Download/{id}')
+    except Exception as e:
+        print(f'{e}')
+
+    print(f'{colorama.Fore.GREEN}> Baixando {mangaName} -> {url}{colorama.Fore.BLUE}')
+    navegador = webdriver.Chrome(options=chrome_options)
+    navegador.get(url)
+    imagens = navegador.find_elements(By.TAG_NAME, 'img')
+
+    lista_images = []
+    contador = 1
+    for img in imagens:
+
+        if servidor == 'unionmanga':
+            link = img.get_attribute('src')
+        if servidor == 'mangachan':
+            link = img.get_attribute('data-src')
+
+        print(link)
+        try:
+            download_image(id, link, f'{mangaName}_{contador}.jpg')
+
+        except Exception as e:
+            print(f'erro {e}')
+            try:
+                os.remove(f'Download/{id}/{mangaName}_{contador}.jpg')
+            except:
+                pass
+        contador += 1
+    navegador.quit()
+
+
+def download_image(id, link, img):
+    folder = f'Download/{id}/'
+    with open(folder + '/' + img, 'wb') as handle:
+        response = requests.get(link, stream=True)
+        if not response.ok:
+            response
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+
+
+def create_zip_files(id, manganame, max_size):
+    file_list = []
+    current_size = 0
+    zip_count = 1
+    arquivos_compactados = []
+    path = [os.path.join(p, file) for p, _, files in os.walk(
+        os.path.abspath(f"Download/{id}/")) for file in files]
+
+    lista = natsorted(path)
+    print(lista)
+
+    # Lista todos os arquivos da pasta em ordem alfabética
+
+    for file in lista:
+        # Verifica se o arquivo é uma imagem JPEG
+        if file.lower().endswith('.jpg'):
+            file_size = os.path.getsize(file)
+
+            # Verifica se o tamanho do arquivo excede o limite
+            if current_size + file_size > max_size:
+                # Cria um novo arquivo ZIP
+                zip_filename = f'Baixados/{manganame}_{zip_count}.cbr'
+                with zipfile.ZipFile(zip_filename, 'w') as zipf:
+                    # Adiciona os arquivos à nova pasta ZIP em ordem alfabética
+                    for f in sorted(file_list):
+                        zipf.write(f, arcname=os.path.basename(f))
+                arquivos_compactados.append(
+                    f'Baixados/{manganame}_{zip_count}.cbr')
+                # Reseta as variáveis para o próximo arquivo ZIP
+                file_list = []
+                current_size = 0
+                zip_count += 1
+
+            # Adiciona o arquivo atual à lista de arquivos
+            file_list.append(file)
+            current_size += file_size
+
+    # Cria o último arquivo ZIP, se houver algum arquivo restante
+    if file_list:
+        arquivos_compactados.append(f'Baixados/{manganame}_{zip_count}.cbr')
+        zip_filename = f'Baixados/{manganame}_{zip_count}.cbr'
+        with zipfile.ZipFile(zip_filename, 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
+            for f in sorted(file_list):
+                zipf.write(f, arcname=os.path.basename(f))
+    return arquivos_compactados
+
+
+with open('recente.txt', 'a', encoding='utf-8') as f:
+    with open('recente.txt', 'r', encoding='utf-8') as fr:
         fr = fr.read()
 
         for i in links:
             manga = getCapitulosFromUrl(i)
             if not manga in fr:
-                f.write(manga)
+                limpaArquivos('saiu hoje', 'Download', 'jpg')
+                f.write(manga+'\n')
                 for u in envio:
-                    bot.sendMessage(int(u), f'{manga}\n')
+                    print(u)
+                    if 'unionleitor' in manga:
+                        servidor = 'unionmanga'
+                    else:
+                        servidor = 'mangachan'
+                    nome = f"{manga.split(' ')[0].split('/')[-2]}_{manga.split(' ')[0].split('/')[-1]}"
+                    print(nome)
+                    getImgFromUrl('saiu hoje', nome, servidor,
+                                  manga.split(' ')[0])
+                    zipa = create_zip_files(
+                        'saiu hoje', f'{nome}', max_size=45 * 1024 * 1024)
+                    for i in zipa:
+                        bot.sendMessage(int(u), f'Acabou de sair... {nome}')
+                        bot.sendDocument(int(u), document=open(i, 'rb'))
+                    # bot.sendMessage(int(u), f'{manga}\n')
+
+limpaArquivos('saiu hoje', 'Download', 'jpg')
---
+++ teste.py
@@ -0,0 +1,2 @@
+link = 'https://unionleitor.top/manga/chainsaw-man,Cap. 135'
+print(link.split(' ')[0].split('/')[-2])
